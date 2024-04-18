426c6f02e553fe324a4836e118c674d97edc6e2f
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-18 12:23:34 +0530

    updated componenet with mapping and states

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,55 @@
-import React from "react";
-import Header from "./Header";
+import React, { useState } from "react";
+import {Header} from "./Header";
 import "./App.css"
-import  Footer  from "./Footer"
+import Footer from "./Footer"
 import CreateNote from "./CreateNote";
-import {Note} from "./Note"
+import { Note } from "./Note"
 
 
 const App = () => {
+
+    const [additems, setAddItems] = useState([]);
+
+
+    const addNote = (note) => {
+        setAddItems((preData) => {
+            return [...preData, note]
+        });
+        console.log(note)
+    }
+
+    // const onDelete=(id)=>{
+    //       setAddItems((olddata)=>
+    //       olddata.filter((curdata,indx)=>{
+    //         return  indx !== id;
+    //       }))
+    const onDelete = (id) => {
+        setAddItems((olddata) =>
+            olddata.filter((currdata, indx) => {
+                return indx !== id
+            })
+        )
+    }
+
     return (
         <>
-        <div>
-            <Header />
-            <CreateNote/>
-            <Note/>
-            <Footer/>
-            </div>   
+            <div>
+                <Header />
+                <CreateNote passNote={addNote} />
+                {
+                    additems.map((data, index) => {
+                        return (
+                            <Note title={data.title} contet={data.content}
+                                key={index} id={index} deletItem={onDelete} />
+
+                        )
+                    })
+
+                }
+
+
+                <Footer />
+            </div>
         </>
     )
 }
