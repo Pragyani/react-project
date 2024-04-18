88cddf705dccf48875dbe0867314dd5dd57bbf8d
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-18 12:26:47 +0530

    adding: props in the component

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,14 +1,18 @@
 
 
-export const Note = () => {
+export const Note = (props) => {
+
+    const deletedNote=()=>{
+        props.deletItem(props.id)
+    }
+
     return (
         <>
             <div className="note">
-                <h1>Tittle</h1>
+                <h1>{props.title}</h1>
                 <br />
-                <p>this is the content</p>
-                <button className="btn">
-                    -</button>
+                <p>{props.contet}</p>
+                <button className="btn" onClick={deletedNote}>-</button>
             </div>
 
 
