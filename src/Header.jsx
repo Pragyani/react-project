763b7a94c0f59e067364ac14c53714e210a819be
PRAGYA PANDEY <pragyani2004@gmail.com>
2025-01-12 13:33:39 +0530

    ad : props to the header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Img from "./Images/logo";
 
-
- export const Header = () => {
+export const Header = ({ toggleTheme, isDarkMode }) => {
     return (
-        <>
-            <div className="header">
-                <img src={Img} alt="logo" width='90' height='80' />
-                <p>AniPragya Keep</p>
+        <div className="header">
+            <img src={Img} alt="logo" width="90" height="80" />
+            <p>AniPragya Keep</p>
+
+            {/* Theme toggle checkbox */}
+            <div className="theme-toggle">
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={isDarkMode}
+                        onChange={toggleTheme}
+                    />
+                    Dark Mode
+                </label>
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
