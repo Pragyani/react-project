16aad7f70bec20456d938a5dcdf162bc24ff28eb
PRAGYA PANDEY <pragyani2004@gmail.com>
2025-01-14 23:31:07 +0530

    add : Edit icon from react-icons in the Note.jsx

--- src/CreateNote/Note.jsx
+++ src/CreateNote/Note.jsx
@@ -5,12 +5,11 @@ import './createNote.css';
 
 /**External Dependencies */
 import { IoClose } from "react-icons/io5";
-
-
+import { CiEdit } from "react-icons/ci";
 
 export const Note = (props) => {
 
-    const deletedNote=()=>{
+    const deletedNote = () => {
         props.deletItem(props.id)
     }
 
@@ -20,7 +19,10 @@ export const Note = (props) => {
                 <h1>{props.title}</h1>
                 <br />
                 <p>{props.contet}</p>
-                <button className="del-btn" onClick={deletedNote}><IoClose className="deleted"/></button>
+                <div className="notes-icon">
+                    <button className="del-btn" onClick={deletedNote}><IoClose className="deleted" /></button>
+                    <button><CiEdit /></button>
+                </div>
             </div>
         </>
     )
