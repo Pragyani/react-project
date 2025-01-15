54d4a93625fb843760956bcea30462ad1877c881
PRAGYA PANDEY <pragyani2004@gmail.com>
2025-01-15 15:38:14 +0530

    add : EditModal and States to manage the modal visibility.

--- src/CreateNote/Note.jsx
+++ src/CreateNote/Note.jsx
@@ -1,29 +1,49 @@
-/**Internal Dependencies */
-
-import React from "react";
+/**Internal Dependencied */
+import React, { useState } from "react";
 import './createNote.css';
+import EditModal from "../MODAL/EditModal/editModal";
 
-/**External Dependencies */
-import { IoClose } from "react-icons/io5";
-import { CiEdit } from "react-icons/ci";
+/**External dependencies */
+import { AiOutlineDelete } from "react-icons/ai";
+import { MdEdit } from "react-icons/md";
 
 export const Note = (props) => {
+    const [editbox, setEditbox] = useState(false);
+    const [noteContent, setNoteContent] = useState({ title: props.title, content: props.contet });
 
-    const deletedNote = () => {
-        props.deletItem(props.id)
-    }
+    const deletedNote = () => { props.deletItem(props.id); };
+
+    const openEditbox = () => { setEditbox(true); };
+
+    const closeEditbox = () => { setEditbox(false); };
+
+    const handleSaveEdit = (newTitle, newContent) => {
+        setNoteContent({ title: newTitle, content: newContent });
+    };
 
     return (
         <>
             <div className="note">
-                <h1>{props.title}</h1>
-                <br />
-                <p>{props.contet}</p>
                 <div className="notes-icon">
-                    <button className="del-btn" onClick={deletedNote}><IoClose className="deleted" /></button>
-                    <button><CiEdit /></button>
+                    <button className="del-btn" onClick={deletedNote}>
+                        <AiOutlineDelete className="deleted" />
+                    </button>
+                    <button className="E-btn" onClick={openEditbox}>
+                        <MdEdit className="edit-btn" />
+                    </button>
                 </div>
+                <h1>{noteContent.title}</h1>
+                <p>{noteContent.content}</p>
             </div>
+
+            {editbox && (
+                <EditModal
+                    title={noteContent.title}
+                    content={noteContent.content}
+                    onClose={closeEditbox}
+                    onSave={handleSaveEdit}
+                />
+            )}
         </>
-    )
-}
+    );
+};
