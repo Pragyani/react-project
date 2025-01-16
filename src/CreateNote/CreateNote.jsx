fab495d36090c84d3aab60361f9df5fb142cba2b
PRAGYA PANDEY <pragyani2004@gmail.com>
2025-01-16 10:50:34 +0530

    add : multiple chnages

--- src/CreateNote/CreateNote.jsx
+++ src/CreateNote/CreateNote.jsx
@@ -46,14 +46,14 @@ const CreateNote = (props) => {
             <div className="main_note">
                 <form onClick={fromField} onDoubleClick={backToNormal}>
                     {expand ?
-                        <input type="text" placeholder="Tittle"
+                        <input type="text" placeholder="Tittle :"
                             autoComplete="off" value={note.title} onChange={inputEvent} name="title" /> : null}
 
-                    <textarea placeholder="Write A Note" rows=''
+                    <textarea placeholder="Write your note:" rows=''
                         column='' value={note.content} onChange={inputEvent}
                         name="content" onClick={expandIt}></textarea>
 
-                    {expand ? <button onClick={addEvent} className="add-btn"> + </button> : null}
+                    {expand ? <button onClick={addEvent} className="add-btn">+ Add to Note </button> : null}
 
                 </form>
             </div>
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,32 +1,47 @@
-/**Internal Dependencies */
+/**internal dependencies */
 import React from "react";
 import Img from "../Images/logo";
 import './header.css';
 
-/** External Deendencies.*/
-import { BiSolidDashboard } from "react-icons/bi";
+/** External Dependencies. */
+import { IoMdSettings } from "react-icons/io";
 import { FaUserLarge } from "react-icons/fa6";
-import { IoMdRefreshCircle } from "react-icons/io";
+import { RiResetRightFill } from "react-icons/ri";
 import { MdDarkMode } from "react-icons/md";
+import { TfiViewList } from "react-icons/tfi";
 
 export const Header = ({ toggleTheme, isDarkMode }) => {
     return (
-        <div className="header">
+        <div className={`header ${isDarkMode ? 'dark' : 'light'}`}>
             <img src={Img} alt="logo" width="90" height="80" />
             <p>AniPragya Keep</p>
-
             <ul>
-                <li> <IoMdRefreshCircle/></li>
-                <li><BiSolidDashboard/></li>
-                <li><FaUserLarge/></li>
-                <div className="theme-toggle">
-                    <label>
-                        <input type="checkbox" checked={isDarkMode} onChange={toggleTheme}  className="chckbox"/>
-                      <li><MdDarkMode/></li>
-                    </label>
-                </div>
+                <li className="icon-item">
+                    <IoMdSettings />
+                    <span className="tooltip">Settings</span>
+                </li>
+                <li className="icon-item">
+                    <RiResetRightFill />
+                    <span className="tooltip">Reset</span>
+                </li>
+                <li className="icon-item">
+                    <FaUserLarge />
+                    <span className="tooltip">User</span>
+                </li>
+                <li className="icon-item">
+                    <TfiViewList />
+                    <span className="tooltip">View List</span>
+                </li>
+                <li className="icon-item">
+                    {/* Dark Mode Toggle */}
+                    <MdDarkMode
+                        onClick={toggleTheme}
+                        style={{ cursor: 'pointer' }}
+                        title={isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+                    />
+                    <span className="tooltip">{isDarkMode ? "Light Mode" : "Dark Mode"}</span>
+                </li>
             </ul>
-
         </div>
     );
 };
