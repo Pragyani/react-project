4015cc095ce8d269a169c714a6f7e012c7ede255
PRAGYA PANDEY <pragyani2004@gmail.com>
2025-01-16 22:09:00 +0530

    add : Provider and store in the index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App from './App.jsx'
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './reducer/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+ <App />
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
